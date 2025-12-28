c0cf069ff3485377294d58ae6dc1e532f9b34fa0
GautamNimesh <np03cs4a240001@heraldcollege.edu.np>
2025-12-28 13:02:00 +0545

    Update Utils class from branchA

--- Utils.java
+++ Utils.java
@@ -3,5 +3,6 @@ package Week7Tut;
 public class Utils {
 	public static void main(String[] args) {
 		System.out.println("Hello from Utils file");
+		System.out.println("Update Util classfrom branchA");
 	}
 }
