ea108b054e987b6afa067ae9bc0cfd707aea79fe
GautamNimesh <np03cs4a240001@heraldcollege.edu.np>
2025-12-30 13:18:08 +0545

    Added getter methods to Student class

---
+++ Student.java
@@ -0,0 +1,11 @@
+package Workshop_week_7;
+
+public class Student {
+	private String name;
+	private int age;
+	
+	public Student(String name, int age){
+		this.age = age;
+		this.name = name;
+	}
+}
